0dbb1b3f018a6ff36e2c103136d4e5a26def223c
Shadwal Sinha <mailtoshadwal@gmail.com>
2022-07-20 13:43:49 +0530

    Change6

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, TitleStrategy } from '@angular/router';
+import{faSearch} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-search',
@@ -7,6 +8,7 @@ import { ActivatedRoute, Router, TitleStrategy } from '@angular/router';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  search1= faSearch;
   searchItem: string='';
   constructor(private route:ActivatedRoute, private router: Router) { }
 
